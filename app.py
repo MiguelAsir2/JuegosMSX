db7405ee87b9d616c158f6e1dfdd6d087b8112bf
Miguel <miguelcordobaaranda@gmail.com>
2020-05-13 14:14:48 +0200

    Comento Mejoras

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask,render_template,abort,request
 app= Flask(__name__)
 
 import json
+import os
 with open("MSX.json") as fichero:
     datos=json.load(fichero)
 
@@ -26,6 +27,7 @@ def juegos():
         categoria=[]
         for dato in datos:
             todos_juegos.append(dato)
+            #Mejora 3: Añadir la busqueda por Categoria.
             if nombre in str(dato["nombre"]) and dato["categoria"] not in categoria :
                 lista_juegos.append(dato)
                 categoria.append(dato["categoria"])
@@ -39,4 +41,5 @@ def juego(id):
             juego.append(dato)
     return render_template("juego.html",datos=juego)
 
-app.run(debug=True)
+port=os.environ["PORT"]
+app.run('0.0.0.0', int(port), debug=False)
